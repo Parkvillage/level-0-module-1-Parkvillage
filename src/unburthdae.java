d41c59545ea9715d360c770bea571bcade358cb1
League <League@adminisorsimac2.attlocal.net>
2018-06-16 12:58:47 -0700

    hi

---
+++ src/unburthdae.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class unburthdae {
+	public static void main(String[]args) {
+	String fluf=JOptionPane.showInputDialog(null,"whens yo burthdae? (mm/dd)");
+	if(fluf.equals("6/15")) {
+		JOptionPane.showInputDialog(null,"happy birthday!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		
+	}else {
+		JOptionPane.showMessageDialog(null,"happy UNBIRTHDAE");
+	}
+	}
+}
